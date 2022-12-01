33566bc62dfb33675d910aeb6e058d0d9ace2ad9
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-12-01 09:09:20 +0100

    day 1

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,8 +1,103 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Context, Result};
 
 fn main() -> Result<()> {
     let input = libaoc::init()?;
+    let calories = parse_input(&input)?;
+    let max = find_max(&calories);
 
-    dbg!(input);
+    println!("The elf having the most calories has {max} calories");
+
+    let top_n = 3;
+    let total = find_total(&calories, top_n);
+    println!("The top {top_n} elves have {total} calories in total");
     Ok(())
+}
+
+type Calories = Vec<Vec<i32>>;
+
+fn parse_input(input: &str) -> Result<Calories> {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|e| {
+            e.trim()
+                .split('\n')
+                .map(|n| n.parse().wrap_err_with(|| format!("Not an int {:?}", n)))
+                .collect::<Result<Vec<i32>>>()
+        })
+        .collect::<Result<Vec<_>>>()
+}
+
+fn find_max(calories: &Calories) -> i32 {
+    find_total(calories, 1)
+}
+
+fn find_total(calories: &Calories, top_n: usize) -> i32 {
+    let mut sorted = calories.clone();
+    sorted.sort_by_key(|e| -e.iter().sum::<i32>());
+    sorted
+        .iter()
+        .take(top_n)
+        .map(|e| e.iter().sum::<i32>())
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+    use rstest::*;
+
+    use super::*;
+
+    #[fixture]
+    fn input() -> &'static str {
+        indoc! {"
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+        "}
+    }
+
+    #[fixture]
+    fn calories() -> Calories {
+        vec![
+            vec![1000, 2000, 3000],
+            vec![4000],
+            vec![5000, 6000],
+            vec![7000, 8000, 9000],
+            vec![10000],
+        ]
+    }
+
+    #[rstest]
+    fn test_parse(input: &str, calories: Calories) {
+        let parsed = parse_input(input).unwrap();
+        assert_eq!(parsed, calories);
+    }
+
+    #[rstest]
+    fn test_most_calories(calories: Calories) {
+        let max = find_max(&calories);
+
+        assert_eq!(max, 24000);
+    }
+
+    #[rstest]
+    fn test_top_n_calories(calories: Calories) {
+        let total = find_total(&calories, 3);
+
+        assert_eq!(total, 45000);
+    }
 }
